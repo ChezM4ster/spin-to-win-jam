2159cb88a9e3418609790db91e1cc14fd742aca7
F3stus-0 <ojedaceballosluisdavid@gmail.com>
2026-08-15 10:59:12 -0500

    I dunno
    I opened GitHub a month after the jam ended and this appeared

--- Scripting/main_menu.gd
+++ Scripting/main_menu.gd
@@ -8,3 +8,7 @@ func _on_button_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/controls.tscn")
